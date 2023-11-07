b7dab7f4155cd14baae1d93bf473c58770ecfbd0
Locz3k <83208428+FilipMich@users.noreply.github.com>
2023-11-07 11:59:45 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const middleCursor = document.querySelector(".middle-cursor");
-const Cursor = () => {
+
+const Cursor = ({className}) => {
   const [mousePosition, setMousePosition] = useState({x: 0, y: 0});
   const [shown, setShown] = useState(false);
   useEffect(() => {
@@ -14,13 +15,16 @@ const Cursor = () => {
     window.addEventListener("mousemove", updateMousePosition);
     if(mousePosition.x < window.innerWidth && mousePosition.y < window.innerHeight && mousePosition.x > 0 && mousePosition.y > 0) (window.innerWidth >= 1000 ? setShown(true) : setShown(false));
   });
-  return (shown && (<div className="cursor" style={{top: `${mousePosition.y-40}px`, left: `${mousePosition.x-40}px`}}></div>))
+  return (shown && (<div className={"cursor " + className} style={{top: `${mousePosition.y-(className ? 50 : 40)}px`, left: `${mousePosition.x-(className ? 50 : 40)}px`}}></div>))
 }
 
-root.render(
+const Page = () => {
+  const [cursorState, setCursorState] = useState();
+  
+  return (
     <div className="container">
-        <Cursor />
-        <header>
+        <Cursor className={cursorState}/>
+        <header onMouseEnter={() => {setCursorState("invert-colors")}} onMouseLeave={() => {setCursorState(null)}}>
           <h1>
             Hey there, I’m <span style={{color: "#4831D4"}}>Filip</span><br/>
             Full-Stack Developer
@@ -29,4 +33,9 @@ root.render(
         </header>
         <h3>I'm currently working on my portfolio. Come back later!</h3>
     </div>
+  )
+}
+
+root.render(
+    <Page />
 );
